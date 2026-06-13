37e0ccf59f06f9bc828d82cd531fd3e1eaaa74a5
Prem Sachdave <premkumarr238@gmail.com>
2026-06-13 11:15:19 +0500

    add skill class in schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -15,3 +15,16 @@ class CareerPathResponse(CareerPathBase):
     class Config:
         from_attributes = True
 
+class SkillBase(BaseModel):
+    name: str
+    career_path_id: int
+    level: Optional[str] = None
+
+class SkillCreate(SkillBase):
+    pass
+
+class SkillResponse(SkillBase):
+    id: int
+    created_at:datetime
+    class Config:
+        from_attributes = True
